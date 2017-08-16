dd604edbe614c96b4580ac1107792b510556ccd1
Ankur Agarwal <signup.ankur@gmail.com>
2017-08-16 16:53:00 +0530

    Modified production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,6 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
-
   config.action_mailer.delivery_method = :ses
   config.action_mailer.default_url_options = { host: "https://bequest.herokuapp.com"}
 
