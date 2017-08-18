171b83c432bc8da8088a9ca7100187877ca6d58e
Ankur Agarwal <signup.ankur@gmail.com>
2017-08-19 00:04:02 +0530

    Updated the host for the Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   config.action_mailer.delivery_method = :ses
-  config.action_mailer.default_url_options = { host: "https://bequest.herokuapp.com"}
+  config.action_mailer.default_url_options = { host: "https://bequestlife.com"}
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
