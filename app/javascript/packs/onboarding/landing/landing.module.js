3b1e68aefe749a37c6b954b2ec75f9bbb2f12c2a
Matt <matt@matts-macbook-pro.attlocal.net>
2017-08-18 14:41:20 -0400

    added loading spinner html

--- app/javascript/packs/onboarding/landing/landing.module.js
+++ app/javascript/packs/onboarding/landing/landing.module.js
@@ -1,10 +1,8 @@
 import angular from 'angular';
 import landingDirective from './landing.directive';
-// import headerDirective from '../header/header.directive';
 
 let landingModule = angular
   .module('landingModule', [])
   .directive('landing', landingDirective);
-  // .directive('header', headerDirective);
 
 export default landingModule.name;
