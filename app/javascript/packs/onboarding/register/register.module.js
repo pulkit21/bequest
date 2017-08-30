09484b9f7277c5d19c1534a38344639948f05065
Matt <matt@matts-macbook-pro.attlocal.net>
2017-08-29 22:23:20 -0400

    register page css wip

--- app/javascript/packs/onboarding/register/register.module.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import registerDirective from './register.directive';
-
-let registerModule = angular
-  .module('registerModule', [])
-  .directive('register', registerDirective)
-
-export default registerModule.name;
