8fc9e6e6484bf38ac7eee3ced2081643a6668f5b
Matt <matt@matts-macbook-pro.attlocal.net>
2017-10-09 22:09:07 -0400

    application wip

--- app/models/beneficiary.rb
+++ app/models/beneficiary.rb
@@ -2,5 +2,5 @@ class Beneficiary < ApplicationRecord
   belongs_to :insurance
   validates_presence_of :first_name, :last_name, :relation, :birthday, :allocated_percentage
 
-  enum relation: ["Spouse", "Daughter", "Son", "Mother", "Father", "Sibling", "Gaurdian"]
+  enum relation: ["Spouse", "Daughter", "Son", "Mother", "Father", "Sibling", "Gaurdian", "Pet", "Charity"]
 end
